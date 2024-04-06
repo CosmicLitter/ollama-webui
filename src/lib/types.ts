9ce974bc736bfca47bc54ba89de70a62ea7b9061
Brandon <holtbd91@gmail.com>
2024-04-06 10:45:21 -0400

    Ollama types

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,3 +1,20 @@
+export type OllamaResponse = {
+	created_at: string;
+	done: Boolean;
+	model: string;
+	message: OllamaChatMessage;
+	context?: Number[];
+	eval_count?: Number;
+	eval_duration?: Number;
+	prompt_eval_count?: Number;
+	prompt_eval_duration?: Number;
+	total_duration?: Number;
+};
+
+export type OllamaChatMessage = {
+	role: string;
+	content: string;
+};
 export type ChatMessage = {
 	id: number;
 	createdAt: Date;

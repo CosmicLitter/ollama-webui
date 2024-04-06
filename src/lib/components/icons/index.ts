7629286baf471b7a6179fe4b6d69e0e62827291a
Brandon <holtbd91@gmail.com>
2024-04-06 10:29:47 -0400

    Add new icons and spinner component

--- src/lib/components/icons/index.ts
+++ src/lib/components/icons/index.ts
@@ -2,11 +2,17 @@ import { SvelteComponent } from 'svelte';
 import { MessageSquarePlus } from 'lucide-svelte';
 import { Bot } from 'lucide-svelte';
 import { CircleUserRound } from 'lucide-svelte';
+import { Send } from 'lucide-svelte';
+import { UserRoundIcon } from 'lucide-svelte';
 import Ollama from './ollama.svelte';
+import Spinner from './spinner.svelte';
 
 export type Icon = SvelteComponent;
 
 export const Icons = {
+	userAvatar: UserRoundIcon,
+	spinner: Spinner,
+	send: Send,
 	ollama: Ollama,
 	newChat: MessageSquarePlus,
 	bot: Bot,
